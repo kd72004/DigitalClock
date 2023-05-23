f1d93f3b5e17068de5f8420bcd254ab870f523e8
Kalyani Dave <109151753+kd72004@users.noreply.github.com>
2023-05-23 21:54:28 +0530

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hours=document.getElementById('hour');
 const minutes=document.getElementById('minutes');
 const seconds=document.getElementById('seconds');
+const amPm=document.getElementById('amPm');
 
 function updateHour() {
 // Get current time
@@ -10,7 +11,8 @@ var currentTime = new Date();
 let hours_var = currentTime.getHours();
 let minutes_var = currentTime.getMinutes();
 let seconds_var = currentTime.getSeconds();
-
+var period = hours_var < 12 ? "AM" : "PM";
+ 
 // Pad single-digit values with leading zeros
 hours_var = (hours_var < 10 ? "0" : "") + hours_var;
 minutes_var = (minutes_var < 10 ? "0" : "") + minutes_var;
@@ -21,6 +23,7 @@ seconds_var = (seconds_var < 10 ? "0" : "") + seconds_var;
 hours.innerHTML = hours_var;
 minutes.innerHTML = minutes_var;
 seconds.innerHTML = seconds_var;
+amPm.innerHTML=period;
 }
 // Set interval to update time
 
