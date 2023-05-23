ac85ec9496ee3077287a5b63dc04c9ed5ba55dd4
Kalyani Dave <109151753+kd72004@users.noreply.github.com>
2023-05-23 21:41:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const hours=document.getElementById('hour');
+const minutes=document.getElementById('minutes');
+const seconds=document.getElementById('seconds');
+
+function updateHour() {
+// Get current time
+var currentTime = new Date();
+
+// Extract hours, minutes, and seconds
+let hours_var = currentTime.getHours();
+let minutes_var = currentTime.getMinutes();
+let seconds_var = currentTime.getSeconds();
+
+// Pad single-digit values with leading zeros
+hours_var = (hours_var < 10 ? "0" : "") + hours_var;
+minutes_var = (minutes_var < 10 ? "0" : "") + minutes_var;
+seconds_var = (seconds_var < 10 ? "0" : "") + seconds_var;
+
+// Display the current time
+
+hours.innerHTML = hours_var;
+minutes.innerHTML = minutes_var;
+seconds.innerHTML = seconds_var;
+}
+// Set interval to update time
+
+setInterval(updateHour, 1000);
